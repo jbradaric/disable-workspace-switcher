e4b8d3ea6d81be4fbb03839c7d0922087aaf930a
Jurica Bradaric <jbradaric@gmail.com>
2023-10-17 17:58:29 +0200

    Support gnome shell 45

--- disable-workspace-switcher@jbradaric.me/extension.js
+++ disable-workspace-switcher@jbradaric.me/extension.js
@@ -1,21 +1,14 @@
-'use strict'
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import { WorkspaceSwitcherPopup } from 'resource:///org/gnome/shell/ui/workspaceSwitcherPopup.js';
 
-const WorkspaceSwitcherPopup = imports.ui.workspaceSwitcherPopup.WorkspaceSwitcherPopup;
-
-class Extension {
-    constructor() {
-        this.originalWorkspaceSwitcherPopup_display = WorkspaceSwitcherPopup.prototype.display;
-    }
-
+export default class extends Extension {
     enable() {
+        this._originalWorkspaceSwitcherPopup_display = WorkspaceSwitcherPopup.prototype.display;
         WorkspaceSwitcherPopup.prototype.display = () => {};
     }
 
     disable() {
-        WorkspaceSwitcherPopup.prototype.display = this.originalWorkspaceSwitcherPopup_display;
+        WorkspaceSwitcherPopup.prototype.display = this._originalWorkspaceSwitcherPopup_display;
+        this._originalWorkspaceSwitcherPopup_display = null;
     }
-}
-
-function init() {
-    return new Extension();
 }
